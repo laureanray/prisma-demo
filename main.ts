7c6e6e7085098fe46d32a34d2264c7a270675578
Laurean Ray Bahala <laurean.bahala@quasset.com>
2021-09-16 22:28:50 +0800

    fix

--- main.ts
+++ main.ts
@@ -3,17 +3,12 @@ import { PrismaClient, User } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  console.log('main');
-
   const result: User = await prisma.user.create({
     data: {
       email: 'laurean.bahala@quasset.com',
       name: 'Laurean Ray'
     }
   })
-
-  console.log('Created user', result);
-
 }
 
 main()
