7739de5c61c85533f2629b34cc269fe0514150e0
Laurean Ray Bahala <laurean.bahala@quasset.com>
2021-09-17 13:11:40 +0800

    demo update

--- main.ts
+++ main.ts
@@ -1,14 +1,144 @@
-import { PrismaClient, User } from '@prisma/client'
+import { PrismaClient, User, Post } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
 async function main() {
-  const result: User = await prisma.user.create({
+
+  // Create
+  const user = await prisma.user.create({
     data: {
-      email: 'laurean.bahala@quasset.com',
-      name: 'Laurean Ray'
+      email: 'example@example.com',
+      name: 'John Doe'
     }
   })
+
+
+  console.log('Created user', user);
+
+
+
+
+
+
+
+
+
+  // Create + Connect to existing record
+  // const post = await prisma.post.create({
+  //   data: {
+  //     title: 'Prisma is dope!',
+  //     content: 'Some content',
+  //     published: true,
+  //     author: {
+  //       connect: {
+  //         id: user.id
+  //       }
+  //     }
+  //   }
+  // })
+  // console.log('Created post', post);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Nested writes
+  // const user1 = await prisma.user.create({
+  //   data: {
+  //     email: 'laurean.bahala@quasset.com',
+  //     name: 'Laurean Ray',
+  //     posts: {
+  //       create: [
+  //         {
+  //           title: 'Prisma is cool',
+  //           content: 'Some content',
+  //           published: true
+  //         },
+  //         {
+  //           title: 'Make database easy with Prisma',
+  //           content: 'Some content',
+  //           published: true
+  //         }
+  //       ]
+  //     }
+  //   }
+  // })
+  // console.log('Created user with post', user1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Get record by unique identifier
+  // const user2 = await prisma.user.findUnique({
+  //   where: {
+  //     id: user.id
+  //   }
+  // })
+  // console.log('Found user', user2);
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Update record
+  // const updatedUser = await prisma.user.update({
+  //   where: {
+  //     email: 'laurean.bahala@quasset.com'
+  //   },
+  //   data: {
+  //     name: 'LR'
+  //   }
+  // })
+  // console.log('Updated user', updatedUser);
 }
 
 main()
